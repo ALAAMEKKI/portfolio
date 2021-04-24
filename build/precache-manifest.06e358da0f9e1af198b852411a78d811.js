80f66c1eb426c2976c6de35d0daf693c3c9be908
ALAAMEKKI <alaamekki01@gmail.com>
2021-04-25 00:50:30 +0100

    finalv1.0

---
+++ build/precache-manifest.06e358da0f9e1af198b852411a78d811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b723d907fe4ba79cd9d9473c3e8c63a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d159ffaab10626bd28e",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "90abeaa56a020120150b",
+    "url": "/static/css/main.840d88e3.chunk.css"
+  },
+  {
+    "revision": "9d159ffaab10626bd28e",
+    "url": "/static/js/2.48819861.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.48819861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90abeaa56a020120150b",
+    "url": "/static/js/main.96c9f5da.chunk.js"
+  },
+  {
+    "revision": "1535ba1dc7849a9b90e4",
+    "url": "/static/js/runtime-main.4220ea81.js"
+  }
+]);
